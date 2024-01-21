43db172bd7631e47eb0587e442b3b01363bec94e
lukasschirren <l.schirren@web.de>
2024-01-21 12:47:53 +0000

    First model is working

--- least_cost.jl
+++ least_cost.jl
@@ -79,3 +79,170 @@ dispatch_scale = 8760/length(T)
 
 ### model ### 
 m = Model(Clp.Optimizer)
+
+@variables m begin
+    # variables dispatch (include heat)
+    G[DISP, T] >= 0
+    CU[T] >= 0
+    D_stor[S,T] >= 0
+    L_stor[S,T] >= 0
+
+    # variables investment model
+    CAP_G[P] >= 0
+    CAP_D[S] >= 0
+    CAP_L[S] >= 0
+end
+
+@objective(m, Min,
+    sum(vc[disp] * G[disp,t] for disp in DISP, t in T) * dispatch_scale
+    + sum(ic_generation_cap[p] * CAP_G[p] for p in P)
+    + sum(ic_charging_cap[s] * CAP_D[s] for s in S if haskey(ic_charging_cap, s))
+    + sum(ic_storage_cap[s] * CAP_L[s] for s in S)
+)
+
+
+@expression(m, feed_in[ndisp=NONDISP, t=T], availability[ndisp][t]*CAP_G[ndisp])
+
+@constraint(m, ElectricityBalance[t=T],
+    sum(G[disp,t] for disp in DISP)
+    + sum(feed_in[ndisp,t] for ndisp in NONDISP)
+    - sum(D_stor[s,t] for s in S)
+    - CU[t]
+    ==
+    demand_elec[t])
+
+
+@constraint(m, MaxGeneration[disp=DISP, t=T],
+    G[disp,t] <= CAP_G[disp])
+
+@constraint(m, MaxCharge[s=S, t=T; haskey(ic_charging_cap, s)],
+    D_stor[s,t] <= CAP_D[s])
+
+@constraint(m, SymmetricChargingPower[s=S, t=T; !(haskey(ic_charging_cap, s))],
+    CAP_G[s] == CAP_D[s])
+
+@constraint(m, MaxLevel[s=S, t=T],
+    L_stor[s,t] <= CAP_L[s])
+
+@constraint(m, StorageLevel[s=S, t=T],
+    L_stor[s, successor(T,t)]
+    ==
+    L_stor[s, t]
+    + eff_in[s]*D_stor[s,t]
+    - (1/eff_out[s]) * G[s,t] )
+
+optimize!(m)
+
+objective_value(m)
+
+
+#################################
+## Plots
+#################################
+
+#### Plot ELECTRICITY BALANCE
+
+colordict = Dict(
+    "pv" => :yellow,
+    "heatpumps" => :goldenrod1,
+    "chp" => :dodgerblue1,
+    "battery" => :lightgrey,
+    "demand" => :darkgrey,
+    "curtailment" => :red
+)
+
+
+######## plot electricity balance ###########
+
+result_G = get_result(G, [:technology, :hour])
+result_feed_in = get_result(feed_in, [:technology, :hour])
+
+result_charging = get_result(D_stor, [:technology, :hour])
+result_CU = get_result(CU, [:hour])
+result_CU[!,:technology] .= "curtailment"
+df_demand = DataFrame(hour=T, technology="demand", value=demand_elec)
+
+result_generation = vcat(result_feed_in, result_G)
+result_demand = vcat(result_charging, result_CU, df_demand)
+
+table_gen = unstack(result_generation, :hour, :technology, :value)
+table_gen = table_gen[!,[NONDISP..., DISP...]]
+labels = names(table_gen) |> permutedims
+colors = [colordict[tech] for tech in labels]
+data_gen = Array(table_gen)
+
+balance_plot = areaplot(
+    data_gen,
+    label=labels,
+    color=colors,
+    width=0,
+    leg=:outertopright
+)
+
+table_dem = unstack(result_demand, :hour, :technology, :value)
+table_dem = table_dem[!,["demand", S...,"curtailment"]]
+labels2 = names(table_dem) |> permutedims
+colors2 = [colordict[tech] for tech in labels2]
+replace!(labels2, [item => "" for item in intersect(labels2, labels)]...)
+data_dem = -Array(table_dem)
+
+areaplot!(
+    balance_plot,
+    data_dem,
+    label=labels2,
+    color=colors2,
+    width=0,
+    leg=:outertopright
+)
+
+hline!(balance_plot, [0], color=:black, label="", width=2)
+#################################
+
+
+df_installed_gen = get_result(CAP_G, [:technology])
+x = df_installed_gen[!,:technology]
+y = df_installed_gen[!,:value] ./ 1000
+p1 = bar(
+    x,
+    y,
+    leg=false,
+    title="Installed power generation",
+    ylabel="GW",
+    guidefontsize=8,
+    rotation=45
+)
+
+df_installed_charge = get_result(CAP_D, [:technology])
+x = df_installed_charge[!,:technology]
+y = df_installed_charge[!,:value] ./ 1000
+p2 = bar(
+    x,
+    y,
+    leg=false,
+    title="Installed power charging",
+    ylim=ylims(p1),
+    rotation=45
+)
+
+df_installed_storage = get_result(CAP_L, [:technology])
+x = df_installed_storage[!,:technology]
+y = df_installed_storage[!,:value] ./ 1e6
+
+p3 = bar(
+    x,
+    y,
+    leg=false,
+    title="Installed storage capacity",
+    ylabel="TWh",
+    guidefontsize=8,
+    rotation=45
+)
+
+plot(
+    p1,
+    p2,
+    p3,
+    layout=(1,3),
+    titlefontsize=8,
+    tickfontsize=6
+)
